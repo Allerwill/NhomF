e6601c949a278199fc465c5478fd8c88701fd756
TruongPhanAnh <Anhphan.trix@gmail.com>
2020-08-11 11:51:04 +0700

    1
    1

--- LeTriDuc/10/js/script.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-    $('.location[position="eight"]').addClass("selected");
-    $(".location").click(function(){
-        $(".location").not(this).removeClass("selected");
-        $(this).toggleClass("selected")
-    });
-
-});
